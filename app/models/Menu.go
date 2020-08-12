296146fe158cc6d07c2757d2f6d49325d12c42ab
walter <mywalter1985@gmail.com>
2020-08-12 17:25:19 +0800

    init

--- app/models/Menu.go
+++ app/models/Menu.go
@@ -1,7 +1,7 @@
 package models
 
 type Menu struct {
-	Id          int     `json:"id" xorm:"pk autoincr 'id'"`
-	Title       string    `json:"title" xorm:"comment('主题') index VARCHAR(255)"`
-	Identifier  string    `json:"identifier" xorm:"unique VARCHAR(36)"`
+	Id         int    `json:"id" xorm:"pk autoincr 'id'"`
+	Title      string `json:"title" xorm:"comment('主题') index VARCHAR(255)"`
+	Identifier string `json:"identifier" xorm:"unique VARCHAR(36)"`
 }
